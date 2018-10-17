603444bc450b3d0333ee8339276510bc068685f0
lwj159159 <443469777@qq.com>
2018-10-17 20:03:27 +0800

    第一天上班打卡

--- src/main.js
+++ src/main.js
@@ -1,18 +1,36 @@
 // 入口文件
 import Vue from 'vue'
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+
+//1.2安装路由
+Vue.use(VueRouter)
+
+//2.1导入 vue-resource
+import VueResource from 'vue-resource'
+
+//2.2安装vue-resource
+Vue.use(VueResource)
+
 
 //导入 MUI的样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 //按需导入 Mint-UI 中的组件
-import {Header} from 'mint-ui'
+import {Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
+//1.3导入自己router.js路由模块
+import router from './router.js'
 
 // 导入 App 根组件
 import app from './App.vue'
 
 var vm = new Vue ({
   el:'#app',
-  render: c=> c(app)
+  render: c=> c(app),
+  router //1.4挂载路由对象到VM实力上
 })
